aace2b48abd4f33862113d853c2ffaeea07cb883
Aryan Dantanni <161295571+aryanDantani@users.noreply.github.com>
2025-02-15 18:22:05 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,9 +5,15 @@ import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
+
   <Provider store={store}>
+
     <App />
+
   </Provider>
+
 );
+
 reportWebVitals();
