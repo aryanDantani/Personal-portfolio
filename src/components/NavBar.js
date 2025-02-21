cc4b8223cbdc7cbb403d59ec08ab25e5639e5408
Aryan Dantanni <161295571+aryanDantani@users.noreply.github.com>
2025-02-21 22:40:06 +0530

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,9 +8,7 @@ import { faBars } from "@fortawesome/free-solid-svg-icons";
 const NavBar = ({ activeTab }) => {
 
   const dispatch = useDispatch();
-
   const [linkNav] = useState(["home", "skills", "projects", "contacts"]);
-
   const [statusNav, changeStatusNav] = useState(null);
 
   const toggleNav = () => {
@@ -24,11 +22,9 @@ const NavBar = ({ activeTab }) => {
   
   return (
     <header>
-
       <div className="logo">
         <img src="/logo.png" alt="" /> Portfolio
       </div>
-
       <nav className={statusNav}>
         {linkNav.map((value) => (
           <span
@@ -40,11 +36,9 @@ const NavBar = ({ activeTab }) => {
           </span>
         ))}
       </nav>
-
       <div className="icon-bar" onClick={toggleNav}>
         <FontAwesomeIcon icon={faBars} />
       </div>
-      
     </header>
   );
 };
