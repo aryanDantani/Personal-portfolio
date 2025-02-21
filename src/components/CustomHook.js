1d8a5628b66c2f39e6d4f005d5bb19f085ecba9a
Aryan Dantanni <161295571+aryanDantani@users.noreply.github.com>
2025-02-21 22:38:20 +0530

    Update CustomHook.js

--- src/components/CustomHook.js
+++ src/components/CustomHook.js
@@ -3,26 +3,19 @@ import { useEffect } from "react";
 import { useSelector } from "react-redux";
 
 const CustomHook = (refTab = null, refList = null) => {
-
   const scrollTab = refTab;
-
   const divs = refList;
-
   const activeTab = useSelector((state) => state.activeTab);
 
   useEffect(() => {
-
     if (scrollTab.current.classList.contains(activeTab)) {
       const componentNode = scrollTab.current;
       componentNode.scrollIntoView({ behavior: "smooth" });
     }
-
     if (divs !== null) {
       divs.current.forEach((div) => {
         div.classList.add("animation");
       });
-
-
       const handleScroll = () => {
         const scrollPosition = window.scrollY;
         divs.current.forEach((div) => {
@@ -37,7 +30,6 @@ const CustomHook = (refTab = null, refList = null) => {
 
       window.addEventListener("scroll", handlScroll);
       window.addEventListener("scroll", handleScroll);
-
       // Cleanup function to remove the event listener
       return () => {
         window.removeEventListener("scroll", handleScroll);
