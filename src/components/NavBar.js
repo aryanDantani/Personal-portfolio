fbaf9140667eac0dddf271d7ff2a58100c0cb2b3
Aryan Dantanni <161295571+aryanDantani@users.noreply.github.com>
2025-03-11 23:41:05 +0530

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,39 +6,24 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const NavBar = ({ activeTab }) => {
-
   const dispatch = useDispatch();
-
   const [linkNav] = useState(["home", "skills", "projects", "contacts"]);
-
   const [statusNav, changeStatusNav] = useState(null);
-
   const toggleNav = () => {
-
     changeStatusNav(statusNav === null ? "active" : null);
-
   };
 
   const changeTab = (value) => {
-
     dispatch(changeTabActive(value));
-
     toggleNav();
-
   };
   
   return (
-    
     <header>
-    
       <div className="logo">
-
         <img src="/logo.png" alt="" /> Portfolio
-
       </div>
-
       <nav className={statusNav}>
-
         {linkNav.map((value) => (
           <span
             key={value}
@@ -48,23 +33,16 @@ const NavBar = ({ activeTab }) => {
             {value}
           </span>
         ))}
-
       </nav>
-
       <div className="icon-bar" onClick={toggleNav}>
-          
         <FontAwesomeIcon icon={faBars} />
-
       </div>
-
     </header>
   );
 };
 
 const mapStateToProps = (state) => ({
-
   activeTab: state.activeTab,
-
 });
 
 export default connect(mapStateToProps, { changeTabActive })(NavBar);
