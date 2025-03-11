cb8c6eb30aa238625c19888a84b93dee57c5035d
Aryan Dantanni <161295571+aryanDantani@users.noreply.github.com>
2025-03-11 23:39:51 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,30 +4,19 @@ import CustomHook from "./CustomHook";
 function Home() {
 
   const scrollTab = useRef();
-
   CustomHook(scrollTab);
-
   return (
-
     <section ref={scrollTab} className="home">
-
       <div className="content">
-
         <div className="name">
-
           MY NAME IS <span>ARYAN</span>
-    
         </div>
-    
         <div className="des">
-    
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores
           officiis beatae repellendus rem ullam, ipsam nemo dolorem dolorum illo
           laborum. Ea sed dolor ab qui, doloremque accusantium esse blanditiis
           possimus!
-
         </div>
-
         <a
           href="/democv.pdf"
           target="_blank"
@@ -36,35 +25,20 @@ function Home() {
         >
           Download My CV
         </a>
-            
-      </div>
-            
+      </div
       <div className="avatar">
-            
         <div className="card">
-
           // <img src="/profile.png" alt="" />
-
           <div className="info">
-            
             <div>Full Stack Developer</div>
-
             <div>Aryan Dantani</div>
-
             <div>03/12</div>
-
             <div>Male</div>
-            
           </div>
-
         </div>
-
       </div>
-      
     </section>
-
   );
-
 }
 
 export default Home;
